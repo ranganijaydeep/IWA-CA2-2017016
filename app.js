b8f6395b8cca5143cee5df7de2c8492b95919377
Jaydeep Rangani <ranganijaydeep@yahoo.in>
2020-04-23 20:22:01 +0000

    post and gets setup backend

--- app.js
+++ app.js
@@ -1,27 +1,28 @@
- const express=require('express');
- const app=express();
- const mongoose=require('mongoose');
- const bodyParser=require('body-parser');
- require('dotenv/config');
+const express = require('express');
+const app = express();
+const mongoose = require ('mongoose');
+const bodyParser = require ('body-parser');
+require('dotenv/config');
 
- app.use(bodyParser.json());
-// import routs 
-const postsRoute =require('./routes/posts');
+app.use(bodyParser.json());
+
+//import routes
+const postsRoute = require('./routes/posts');
 
 app.use('/posts', postsRoute);
 
- // routes
-
-app.get('/',(req,res)=>{
-     res.send('we are on home');
+//routes
+app.get('/', (req, res) =>{
+res.send('we are on home');
 });
 
+app.get('/posts', (req, res) =>{
+res.send('we are on posts');
+});
 
-
-// connect to DB
-mongoose.connect(process.env.DB_CONNECT,{ useNewUrlParser: true },
-()=>console.log('connected to DB!')
+//conect to db
+mongoose.connect (process.env.DB_CONNECT, { useNewUrlParser: true },() =>
+console.log('connectd DB!')
 );
-
-// listening to server 
-app.listen(3000);
+//how to listen to the server
+app.listen(3000); 
--- routes/posts.js
+++ routes/posts.js
@@ -1,31 +1,27 @@
 const express = require ('express');
-const router =express.Router();
-const Post =require('../models/Post');
+const router = express.Router();
+const Post = require ('../models/Post');
 
-
-router.get('/',async (req,res) =>{
-    try {
-        const posts= await Post.find();
-        res.jason(posts);
-    }catch(err){
-        res.json({message : err});
+//this get all the posts 
+router.get('/', async(req, res) => {
+try{
+    const posts = await Post.find();
+    res.json(posts);    
+}catch(err){
+        res.json({message:err});
     }
 });
-
-router.post('/', async(req,res)=>{
-    const post =new Post({
+router.post('/', async (req, res) => {
+    const post = new Post({
         title: req.body.title,
         description: req.body.description
     });
-
-    try {
+try{
     const savedPost = await post.save();
     res.json(savedPost);
-    } catch (err) {
-        res.json({message: err});
-    }
-
+}catch(err){
+    res.json({ message: err });
+}
 });
 
-
-module.exports=router;
+module.exports = router;
